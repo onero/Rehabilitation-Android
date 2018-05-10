ebdac65860a6729758556eec002613ff1a108972
Sk0vgaard <Sk0vgaard@live.dk>
2018-05-10 09:38:41 +0200

    #5667 Something works.
    Can play video by clicking play button.

--- app/src/main/java/dk/adamino/rehabilitation/GUI/YoutubeActivity.java
+++ app/src/main/java/dk/adamino/rehabilitation/GUI/YoutubeActivity.java
@@ -1,14 +1,50 @@
 package dk.adamino.rehabilitation.GUI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import dk.adamino.rehabilitation.R;
 
-public class YoutubeActivity extends AppCompatActivity {
+public class YoutubeActivity extends YouTubeBaseActivity {
+
+    private static final String YOUTUBE_ID = "a4NT5iBFuZs";
+    private static final String API_KEY = "AIzaSyAh7ZH9nhpNKapBwAHzoo_da9TIB9__7G4";
+
+    private YouTubePlayerView mYouTubePlayerView;
+    private Button mButton;
+    private YouTubePlayer.OnInitializedListener mOnInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
+
+        mYouTubePlayerView = findViewById(R.id.youtube_player);
+        mButton = findViewById(R.id.play_video);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(YOUTUBE_ID);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYouTubePlayerView.initialize(API_KEY, mOnInitializedListener);
+            }
+        });
     }
 }
