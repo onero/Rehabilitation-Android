108db00e60fe1dcc018508e85c237b5773162199
Sk0vgaard <Sk0vgaard@live.dk>
2018-05-10 09:22:40 +0200

    #5667 Setup
    Implemented Activity and youtube.jar

---
+++ app/src/main/java/dk/adamino/rehabilitation/GUI/YoutubeActivity.java
@@ -0,0 +1,14 @@
+package dk.adamino.rehabilitation.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import dk.adamino.rehabilitation.R;
+
+public class YoutubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+    }
+}
